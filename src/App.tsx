8b4410f1fb38f7508842d6a1e010167fc189d8a1
unknown <abhi.shetti.3@gmail.com>
2025-11-04 15:21:34 +0530

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState<any>([])
+
+  useEffect(()=>{
+axios.get("https://localhost:7298/api/todo/list").then((dt)=>setData(dt.data));
+  },[])
+
+  return (
+    <>
+ {data.map((dt:any)=><><p>{dt.taskId}</p><p>{dt.taskName}</p></>)}
+    </>
+  )
+}
+
+export default App
