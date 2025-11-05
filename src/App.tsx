c6a7376b33a668bb5c6ce270159f6421ca2009b0
unknown <abhi.shetti.3@gmail.com>
2025-11-05 12:39:01 +0530

    b

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 function App() {
   const [data, setData] = useState<any>([])
-  console.log('API U','API I' ,process.env.REACT_APP_API_URL)
+  console.log('API U','API I' )
 
   useEffect(()=>{
 axios.get("https://localhost:7298/api/todo/list").then((dt)=>setData(dt.data));
